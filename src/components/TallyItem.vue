5183ec36769530c68b22fb0ab63a463a70230e31
HYQ <Chelsea@promote.cache-dns.local>
2020-02-21 01:46:07 +0800

    upload prototype

---
+++ src/components/TallyItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="item">\
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TallyItem',
+  components: {
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  
+</style>
---
+++ src/views/Category.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="category">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Category',
+  components: {
+  }
+}
+</script>
